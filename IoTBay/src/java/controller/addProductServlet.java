3564ba2d6be943033fd32f2a8655532fb61d3519
alwin5021 <alwinmoncy0@gmail.com>
2022-05-13 14:46:49 +1000

    css
    css

--- IoTBay/src/java/controller/addProductServlet.java
+++ IoTBay/src/java/controller/addProductServlet.java
@@ -1,21 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Product;
+import model.dao.*;
 
-/**
- *
- * @author alwin
- */
 public class addProductServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        String SKU = request.getParameter("sku");
+        ArrayList<Product> productList = (ArrayList<Product>) session.getAttribute("products");
+        Product productSelection = null;
+        for(Product p : productList){
+            if(SKU.matches(p.getSKU())){
+                productSelection = p;
+            }
+        }
+        
+        session.setAttribute("productSelected",productSelection);
+        session.setAttribute("isEdited", "");
+        
+        request.getRequestDispatcher("editProduct.jsp").include(request, response);
+    }
     
+    //ONLY GETS EXECUTED WHEN EDIT BUTTON IS PRESSED ON FORM.
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        Product p = (Product) session.getAttribute("productSelected");
+
+        String newName = request.getParameter("newName");
+        String newCategory = request.getParameter("newCategory");
+        System.out.println(newName);
+        //double newPrice = (String) request.getAttribute("newPrice");
+        //int newStock = (Integer) r-equest.getAttribute("newStock");
+        
+        
+        try {
+            manager.updateProduct(p.getSKU(),newName,50,5,true,newCategory);
+        } catch (SQLException ex) {
+            Logger.getLogger(EditProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        session.setAttribute("productSelected", null);
+        request.getRequestDispatcher("CatalogueServlet").include(request, response);
+    }
     
+    //functions to edit product which use the manager
 }
